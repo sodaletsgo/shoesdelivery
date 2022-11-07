8c60f098d5cff7479a566b151fa6e6eabf131bdb
sodaletsgo <sodacookie@naver.com>
2022-11-07 02:31:57 +0000

    customer update #1

--- customer/KafkaProcessor.py
+++ customer/KafkaProcessor.py
@@ -3,9 +3,7 @@ from flask_kafka import FlaskKafka
 from threading import Event
 import json
 from config import config_reader
-from PolicyHandler import wheneverOrdered_NotifyKakaotalk
-from PolicyHandler import wheneverOrderCancelled_NotifyKakaotalk
-from PolicyHandler import wheneverShippingStarted_NotifyKakaotalk
+
 
 class StreamHandler():
 	
@@ -15,7 +13,7 @@ class StreamHandler():
 		self.destination = config['destination']
 		self.group_id = config['group_id']
 		self.bootstrap_server = config['bootstrap_servers']
-		self.producer = KafkaProducer(acks=0, compression_type='gzip', bootstrap_servers=[self.bootsrap_server], value_serializer=lambda x: json.dumps(x).encode('utf-8'))
+		self.producer = KafkaProducer(acks=0, compression_type='gzip', bootstrap_servers=[self.bootstrap_server], value_serializer=lambda x: json.dumps(x).encode('utf-8'))
 		self.consumer = FlaskKafka(INTERRUPT_EVENT,
 		         bootstrap_servers=[self.bootstrap_server],
 		         group_id= self.group_id
@@ -29,9 +27,13 @@ streamhandler = StreamHandler()
 
 @streamhandler.consumer.handle(streamhandler.destination)
 def consume(msg):
-    my_json = msg.value.decode('utf8').replace("'", '"')
-    data = json.loads(my_json)
-    if data['eventType'] == "Ordered""OrderCancelled""ShippingStarted":
-        wheneverOrdered_NotifyKakaotalk(data)
-        wheneverOrderCancelled_NotifyKakaotalk(data)
-        wheneverShippingStarted_NotifyKakaotalk(data)
+	from PolicyHandler import wheneverOrdered_NotifyKakaotalk
+	from PolicyHandler import wheneverOrderCancelled_NotifyKakaotalk
+	from PolicyHandler import wheneverShippingStarted_NotifyKakaotalk
+
+	my_json = msg.value.decode('utf8').replace("'", '"')
+	data = json.loads(my_json)
+	if data['eventType'] == "Ordered""OrderCancelled""ShippingStarted":
+		wheneverOrdered_NotifyKakaotalk(data)
+		wheneverOrderCancelled_NotifyKakaotalk(data)
+		wheneverShippingStarted_NotifyKakaotalk(data)
